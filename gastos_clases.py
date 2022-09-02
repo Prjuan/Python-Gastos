f02c8775bb1cc1b5ad9dc62fc6bb6ec61527f9d8
Juan <prjuan18_86@hotmail.com>
2022-09-02 13:34:48 -0300

    funciona radiobutton

--- gastos_clases.py
+++ gastos_clases.py
@@ -1,4 +1,5 @@
 from cgitb import enable
+from distutils.cmd import Command
 from faulthandler import disable
 from tkinter import *
 from turtle import heading
@@ -47,11 +48,11 @@ class Formulario_Gastos:
         self.entrysalida = Entry(self.ventana, textvariable = self.misalida)
         self.entrysalida.place(x=80, y=25)
         self.lbltipodegasto = Label(self.ventana , text = "Tipo de Gasto").place(x=5, y=45)
-        self.radiotipovehiculo = Radiobutton(self.ventana, text="Vehiculos", variable=self.opcion, value=1, command=self.Mostrar)
+        self.radiotipovehiculo = Radiobutton(self.ventana, text="Vehiculos", variable=self.opcion, value=1, command=lambda:[self.Mostrar(),self.Eleccion()])
         self.radiotipovehiculo.place(x=5, y=70)
-        self.radiotipogenerales = Radiobutton(self.ventana, text="Generales", variable=self.opcion, value=2, command=self.Mostrar)
+        self.radiotipogenerales = Radiobutton(self.ventana, text="Generales", variable=self.opcion, value=2, command=lambda:[self.Mostrar(),self.Eleccion()])
         self.radiotipogenerales.place(x=5, y=90)
-        self.radiotipopapeleria = Radiobutton(self.ventana, text="Papeleria", variable=self.opcion, value=3, command=self.Mostrar)
+        self.radiotipopapeleria = Radiobutton(self.ventana, text="Papeleria", variable=self.opcion, value=3, command=lambda:[self.Mostrar(),self.Eleccion()])
         self.radiotipopapeleria.place(x=5, y=110)
         self.menudesplegable = ttk.Combobox(self.ventana, values=["LTO 318", "OCB 012", "ICP 627", "UFC 581", "VAZ 809", "FFC 291", "KTS 665", "WSH 915"], state="disabled")
         self.menudesplegable.place(x=5, y=140)
@@ -65,6 +66,12 @@ class Formulario_Gastos:
         self.btncargar.place(x=10, y=215)
         self.btnsalir = Button(self.ventana,text="Salir", command=self.Salir)
         self.btnsalir.place(x=60, y=215)
+        self.eleccion = self.Eleccion()
+        # self.eleccion = {
+        #                 "self.eleccion1" : self.radiotipovehiculo.cget("text"),
+        #                 "self.eleccion2" : self.radiotipogenerales.cget("text"),
+        #                 "self.eleccion3" : self.radiotipopapeleria.cget("text")
+        # }
         
     def Mostrar(self):
         if self.opcion.get()==1:
@@ -72,36 +79,36 @@ class Formulario_Gastos:
         if self.opcion.get()!=1:
             self.menudesplegable.config(state="disabled")
 
-    # def Abrir(self):
-    #     miConexion=sqlite3.connect("datagastos.db")
-    #     miCursor=miConexion.cursor()
+    def Eleccion(self):
+        if self.opcion.get()==1:
+            self.eleccion = "Vehiculos"
+        if self.opcion.get()==2:
+            self.eleccion = "Generales"
+        if self.opcion.get()==3:
+            self.eleccion = "Papeleria"
+            return self.eleccion
 
-    #     datos=self.mifecha.get(), self.misalida.get(), self.opcion.get(), self.miproveedor.get(), self.miimporte.get()
-        
-    #     miCursor.execute("INSERT INTO GASTOS")
-
-    def Guardar(self):
-        # datos = (self.mifecha.get(), self.misalida.get(), self.opcion.get(), self.miproveedor.get(), self.miimporte.get())
-        # con = sqlite3.connect("bd1.db")
-        # cursor = con.cursor()
-        # sql = "insert into GASTOS (fecha, salida, tipo, vehiculo, proveedor, importe) values(?, ?, ?, ?, ?, ?)"
-        # cursor.execute(sql, datos)
-        # con.commit()
-        # con.close()
+    def Guardar(self):        
         conexion=sqlite3.connect("bd1.db")
         conexion.execute("insert into gastos values (:m_fecha, :m_salida, :m_opcion, :m_proveedor, :m_importe)",
                     {
                         "m_fecha": self.mifecha.get(),
                         "m_salida": self.misalida.get(),
-                        "m_opcion": self.text.get(),
+                        "m_opcion": self.eleccion,
                         "m_proveedor": self.miproveedor.get(),
                         "m_importe": self.miimporte.get()  
                     })
-
         conexion.commit()
         conexion.close()
     
+    # def Eliminar(self):
+    #      conexion=sqlite3.connect("bd1.db")
     
+   
+
+
+
+
 
     def Salir(self):
         sys.exit()      
